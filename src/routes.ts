ea959b818ae4bfc9c049a91c6367cdc493245eeb
Renata <ferreiradefreitasrenata@gmail.com>
2025-04-24 16:44:16 -0300

    Criação ambiente de trabalho inclusão de dependências

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import { request } from "http";
+
+
+export async function routes(fastify:FastifyInstance, options: FastifyPluginOptions) {
+    fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
+return {ok: true}
+    })
+}
